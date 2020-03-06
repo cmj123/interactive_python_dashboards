0c9d4468c83457424142f9d916f2fc8c1ad637ef
Esuabom Dijemeni <e.dijemeni@gmail.com>
2020-03-06 13:42:33 +0000

    Created bubble plot using plotly

---
+++ section03_plotly_basics/bubble_plot.py
@@ -0,0 +1,30 @@
+# import classes, libraries and functions
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Import key data
+df = pd.read_csv('data/mpg.csv')
+print(df.head())
+print(df.columns)
+
+# Create data object
+data = [go.Scatter(x=df['horsepower'],
+                   y=df['mpg'],
+                   text=df['name'],
+                   mode='markers',
+                   marker= dict(
+                        size=df['weight']/100,
+                        color=df['cylinders'],
+                        showscale=True
+                   ))]
+
+# Create layout object
+layout = go.Layout(title='Bubble Chart',
+                   hovermode='closest')
+
+# Create figure object
+fig = go.Figure(data=data, layout=layout)
+
+# Plot the graph
+pyo.plot(fig, filename="bubble_plot.html")
